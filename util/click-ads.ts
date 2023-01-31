f0c258584117b05d2e4a6aeb1f509b4aba6aade9
lauer3912 <lauer3912@gmail.com>
2023-01-31 21:02:18 +0800

    chore: 对于广告而言，乱序循环遍历尝试点击

--- util/click-ads.ts
+++ util/click-ads.ts
@@ -2,26 +2,38 @@ import _ from 'lodash';
 
 export async function clickOneAds(visibleFrameList: any[]) {
 	let clickSuccess = false;
-	try {
-		const frame = _.sample(visibleFrameList);
 
-		// 有必要的话，滚动到该iframe的位置
+	// 对数组进行随机乱序排序
+	visibleFrameList = _.shuffle(visibleFrameList);
+
+	// 循环点击广告
+	for await (const frame of visibleFrameList) {
 		try {
-			const frameElement = await frame.frameElement();
-			await frameElement.scrollIntoViewIfNeeded();
-		} catch (error) { }
+			// 有必要的话，滚动到该iframe的位置
+			try {
+				const frameElement = await frame.frameElement();
+				await frameElement.scrollIntoViewIfNeeded();
+			} catch (error) {
+				// 容错处理，如果该frame没有frameElement，则不进行滚动
+				continue;
+			 }
 
-		// 使用
-		// 查找iframe 中的内部 div 元素带有属性data-google-av-adk，该元素是广告元素
-		const adElementList = await frame.locator('div[data-google-av-adk]').all();
+			// 使用
+			// 查找iframe 中的内部 div 元素带有属性data-google-av-adk，该元素是广告元素
+			const adElementList = await frame.locator('div[data-google-av-adk]').all();
 	
-		// 随机选择一个广告元素
-		const adElement = _.sample(adElementList);
+			// 随机选择一个广告元素
+			const adElement = _.sample(adElementList);
 	
-		// 点击该广告元素
-		await adElement.click();
-		clickSuccess = true;
-	} catch (error) {}
+			// 点击该广告元素
+			await adElement.click();
+
+			clickSuccess = true;
+			break;
+		} catch (error) {}
+	}
+
+
 
 	return clickSuccess;
 }
