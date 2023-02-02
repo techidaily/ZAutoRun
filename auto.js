fd1ae8213add33538badf7e555b33503bb893a49
lauer3912 <lauer3912@gmail.com>
2023-02-02 15:16:09 +0800

    feat: 增加停止条件

--- auto.js
+++ auto.js
@@ -2,6 +2,7 @@
  * 循环执行 npm run script 相应的脚本，前一个脚本执行完毕后，再执行下一个脚本
  * 要实时输出日志，需要使用 spawn，不要使用调度任务
  */
+const fs = require('fs');
 const { exec } = require('child_process');
 const dayjs = require('dayjs');
 const duration = require('dayjs/plugin/duration')
@@ -24,6 +25,11 @@ function run() {
     // 让Node进程休眠 30 秒
     setTimeout(() => {
       console.log('等待 30 秒后，再执行下一个脚本');
+			// 编写判断n.stop文件是否存在，如果存在，则停止执行
+			if (fs.existsSync('./n.stop')) {
+				console.log('检测到n.stop文件，停止执行');
+				return;
+			}
       run();
     }, 30000);
 	});
